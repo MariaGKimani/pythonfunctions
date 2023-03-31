2b488c5a6dde2a838e1d4615e7ef64ded1d74e93
Mariagoretti <mariakimanigoreti@gmail.com>
2023-03-31 15:32:12 +0300

    Functions

---
+++ math.py
@@ -0,0 +1,19 @@
+def  add(a,b):
+    answer =a+b
+    return answer
+
+def subtract(c,d):
+    sub =c - d
+    return sub
+
+def divide(c,d):
+    div =c - d
+    return div
+
+def multiply(c,d):
+    mult =c - d
+    return mult
+
+def remainder(c,d):
+    rem =c - d
+    return rem
